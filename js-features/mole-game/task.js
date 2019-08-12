3d545d3cc4e1422e30b5d5ee0d51a06cde19542b
SunnyPuss <fomhumster@ya.ru>
2019-08-12 22:49:04 +0300

    getHole and ReferenceError

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,32 +1,31 @@
 let dead = 0;
 let lost = 0;
 
-const holes = document.getElementsByClassName(`hole`)
+for (index = 1; index <= 9; index++) {
+    getHole(index);
+    hole.onclick = function () {
+        hole.classList.contains(`hole_has-mole`);
+        if (hole.classList.contains(`hole_has-mole`) == true) {
+            dead++;
+            document.getElementById(`dead`).innerHTML = dead;
+        } else {
+            lost++;
+            document.getElementById(`lost`).innerHTML = lost;
+        }
 
-function getHole (index) {
-    for (index = 1; index < 9; index++) {
-        return hole = document.getElementById(`hole${index}`)
+        if (dead == 10) {
+            alert (`You win!`);
+            dead = 0;
+            lost = 0;
+        } else if (lost == 5) {
+            alert (`You lose!`);
+            dead = 0;
+            lost = 0;
+        }
     }
 }
 
-holes.onclick = function () {
-    getHole(index);
-    hole.className.includes(`hole_has-mole`);
-    if (hole.className.includes(`hole_has-mole`) == true) {
-        dead++;
-        document.getElementById(`dead`).innerHTML = dead;
-    } else {
-        lost++;
-        document.getElementById(`lost`).innerHTML = lost;
-    }
-
-    if (dead == 10) {
-        alert (`You win!`);
-        dead = 0;
-        lost = 0;
-    } else if (lost == 5) {
-        alert (`You lose!`);
-        dead = 0;
-        lost = 0;
-    }
+function getHole(index) {
+    let hole = document.getElementById(`hole${index}`);
+    return hole;
 }
