b7943b581eef436e1dfc6fd2b39b4deab67630cb
SunnyPuss <fomhumster@ya.ru>
2019-08-21 17:55:24 +0300

    slider done

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,28 +2,29 @@ const arrows = document.querySelectorAll(`.slider__arrow`);
 const arrowLeft = arrows[0];
 const arrowRight = arrows[1];
 const images = document.querySelectorAll(`.slider__item`);
-let startPlus = 1;
-let startMinus = 0 - 1;
+let n = 1;
 
 
 arrowRight.onclick = function() {
     const active = document.querySelector(`.slider__item_active`);
     active.classList.remove(`slider__item_active`)
 
-    images[startPlus++].classList.add(`slider__item_active`);
+    n++;
 
-    if (startPlus >= images.length) {
-        startPlus = 0;
+    if (n >= images.length) {
+        n = 0;
     }
+
+    images[n].classList.add(`slider__item_active`);
 }
 
 arrowLeft.onclick = function() {
     const active = document.querySelector(`.slider__item_active`);
     active.classList.remove(`slider__item_active`)
 
-    startMinus--;
-    if (startMinus < 0) {
-        startMinus = images.length - 1;
+    n--;
+    if (n < 0) {
+        n = images.length - 1;
     }
-    images[startMinus].classList.add(`slider__item_active`);
+    images[n].classList.add(`slider__item_active`);
 }
