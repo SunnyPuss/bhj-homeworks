54ec03971add1536941983ce0866ebaef05f8342
Voldemar Crush <fomhumster@ya.ru>
2019-09-15 00:58:37 +0300

    chachachat

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -2,22 +2,10 @@ const widget = document.querySelector(`.chat-widget`);
 const input = document.getElementById(`chat-widget__input`);
 const messages = document.querySelector( '.chat-widget__messages' );
 const roboMessages = [ 
-  `<div class="message">
-  <div class="message__time">22:10</div>
-  <div class="message__text">Добрый день! (нет)</div>
-  </div>`,
-  `<div class="message">
-  <div class="message__time">22:10</div>
-  <div class="message__text">А ваши денежки то тю-тю!</div>
-  </div>`,
-  `<div class="message">
-  <div class="message__time">22:10</div>
-  <div class="message__text">Я пришёл сюда  писать в чатик и обоссывать лица, и, как видишь, в чатик я уже написал.</div>
-  </div>`,
-  `<div class="message">
-  <div class="message__time">22:10</div>
-  <div class="message__text">Я бы иногда мимолётом думал о Вас, но у меня даже ИИ нет, так что сори. 8(</div>
-  </div>`
+  `Добрый день! (нет)`,
+  `А ваши денежки то тю-тю!`,
+  `Я пришёл сюда  писать в чатик и обоссывать лица, и, как видишь, в чатик я уже написал.`,
+  `Я бы иногда мимолётом думал о Вас, но у меня даже ИИ нет, так что сори. 8(`
 ];
 
 console.log (roboMessages.length);
@@ -35,13 +23,15 @@ input.addEventListener(`keypress`, function(e) {
   if (e.keyCode === 13) {
     messages.innerHTML += `
     <div class="message message_client">
-      <div class="message__time">${new Date()}</div>
+      <div class="message__time">${new Date().getHours()}:${new Date().getMinutes()}</div>
       <div class="message__text">${input.value}</div>`
       input.value = ``;
 
       
-      messages.innerHTML += `
-      ${getRobo()}`
+      messages.innerHTML += `<div class="message">
+      <div class="message__time">${new Date().getHours()}:${new Date().getMinutes()}</div>
+      <div class="message__text">${getRobo()}</div>
+      </div>`
       
   }
 }) 
