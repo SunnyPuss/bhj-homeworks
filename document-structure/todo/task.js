5a8182a991aa8f0d758aade71e5eb6d6bd1772fc
Voldemar Crush <fomhumster@ya.ru>
2019-09-17 02:38:18 +0300

    enter add

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -23,3 +23,22 @@ button.addEventListener(`click`, function() {
     
 })
 
+input.addEventListener(`keypress`, function(e) {
+    if (e.keyCode === 13) {
+        taskList.innerHTML += `
+        <div class="task">
+        <div class="task__title">${input.value}</div>
+        <a href="#" class="task__remove">&times;</a>
+        </div>
+        `;
+        input.value = ``;
+        event.preventDefault();
+
+        const cross = document.querySelector(`.task__remove`)
+        const task = document.querySelector(`.task`);
+        cross.addEventListener(`click`, () => {
+            task.remove();
+        })
+    }
+})
+
