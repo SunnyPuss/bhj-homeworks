38a8a855b130a96a69ab0c4038460eb4dd344395
Voldemar Crush <fomhumster@ya.ru>
2019-09-17 02:13:20 +0300

    how to remove

---
+++ document-structure/todo/task.js
@@ -0,0 +1,25 @@
+const input = document.querySelector(`.tasks__input`);
+const button = document.querySelector(`.tasks__add`);
+/*const cross = document.querySelectorAll(`.task__remove`);
+const crossArr = Array.from(cross);*/
+const taskList = document.querySelector(`.tasks__list`);
+
+
+button.addEventListener(`click`, function() {
+    taskList.innerHTML += `
+    <div class="task">
+        <div class="task__title">${input.value}</div>
+        <a href="#" class="task__remove">&times;</a>
+    </div>
+    `;
+    input.value = ``;
+    event.preventDefault();
+
+    const cross = document.querySelector(`.task__remove`)
+    const task = document.querySelector(`.task`);
+    cross.addEventListener(`click`, () => {
+        task.remove();
+    })
+    
+})
+
